786eb47c89711e3cfe1262ad1ac2fb578e6aab1f
sisagir <Sinan_Sagir@brown.edu>
2014-10-09 01:05:25 +0200

    Statistical uncertainty shapes
    “addStatShapes.py” adds statistical shape errors into data card and
    corresponding histograms into “plots.root” file.
    “addStatShapesAllDataCards.py” recreates the individual data cards for
    each signal.
    Note that before adding the stat shapes to data cards, the individual
    data cards has to be created using “makeAllDataCards.py”. Then, run the
    “addStatShapes.py” to get the stat shapes in the signal separation data
    card (with both signals). Finally, run “addStatShapesAllDataCards.py”
    to modify the individual signal data cards for stat shapes.
    In each script, there are input parameters (input files, output files
    and thresholds) that need to be modified accordingly before running.

---
+++ post/addStatShapesAllDataCards.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+from utils import printTable
+
+def readDataCard(filename):
+    f = open(filename, 'rU')
+    lines = f.readlines()
+    f.close()
+    data = []
+    for line in lines:
+    	if line.startswith("----"): data.append(['break'])
+    	elif (line.startswith("Observation") or line.startswith("shapes")): data.append([line.strip().split()[0],'','','']+line.strip().split()[1:])
+    	elif (line.startswith("bin") or line.startswith("process") or line.startswith("rate")): 
+    		data.append([line.strip().split()[0],'']+line.strip().split()[1:])
+    		if line.startswith("bin"): dict1=line.strip().split()[1:]
+    		if (line.startswith("process") and line.strip().split()[1].startswith("Wh")): dict2=line.strip().split()[1:]
+    	elif line.startswith("stat_"): continue
+    	else: data.append(line.strip().split())
+    dict = [item1+'_'+item2 for item1,item2 in zip(dict1,dict2)]
+    return data, dict
+
+def addStatShapes(filename, dictOld):
+    f = open(filename, 'rU')
+    lines = f.readlines()
+    f.close()
+    for line in lines[:50]:
+    	if line.startswith("bin"): dict1=line.strip().split()[1:]
+    	elif (line.startswith("process") and line.strip().split()[1].startswith("Wh")): dict2=line.strip().split()[1:]
+    	else: continue
+    dict = [item1+'_'+item2 for item1,item2 in zip(dict1,dict2)]
+    data = []
+    start = False
+    for line in lines:
+    	if line.startswith("WJetsShape"): 
+    		start = True
+    		continue
+    	if start:
+			dictTemp = {}
+			count = 2
+			for key in dict: 
+				dictTemp[key] = line.strip().split()[count]
+				count += 1
+			datatemp = line.strip().split()[:2]
+			for key in dictOld:
+				datatemp += [dictTemp[key]]
+			data.append(datatemp)
+    return data
+
+def main(inputDCfile, inputDCfile0P, outputDCfile0P, inputDCfile0M, outputDCfile0M):
+	data0P = []
+	dataOld0P, dictOld0P = readDataCard(inputDCfile0P)
+	data0P += dataOld0P
+	dataNew0P = addStatShapes(inputDCfile, dictOld0P)
+	data0P += dataNew0P
+	out0P=open(outputDCfile0P,'w')
+	printTable(data0P,out0P)
+
+	data0M = []
+	dataOld0M, dictOld0M = readDataCard(inputDCfile0M)
+	data0M += dataOld0M
+	dataNew0M = addStatShapes(inputDCfile, dictOld0M)
+	data0M += dataNew0M
+	out0M=open(outputDCfile0M,'w')
+	printTable(data0M,out0M)
+
+if __name__ == "__main__":
+	prefix = "/uscms_data/d3/ssagir/VHbbAnalysis/CMSSW_5_3_3_patch2/src/VHbb/post/plots"
+	prefix += "/BDT_nominalprime/"
+	inputDCfile = prefix+"dataCard_statUnc0_bg0.txt"
+	inputDCfile0P = prefix+"dataCard_0P.txt"
+	inputDCfile0M = prefix+"dataCard_0M.txt"
+	outputDCfile0P = prefix+"dataCard_0P"+inputDCfile.split('/')[-1][8:]
+	outputDCfile0M = prefix+"dataCard_0M"+inputDCfile.split('/')[-1][8:]
+	main(inputDCfile, inputDCfile0P, outputDCfile0P, inputDCfile0M, outputDCfile0M)
