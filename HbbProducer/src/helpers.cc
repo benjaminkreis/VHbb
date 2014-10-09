f1bd490438ded9bdea1ccf65c123cf2e9f55c4b7
Alexx Perloff <aperloff@physics.tamu.edu>
2014-10-09 16:36:35 -0500

    Added a cut on the eta(2.5) and pt(20) of the candidate jets

--- HbbProducer/src/helpers.cc
+++ HbbProducer/src/helpers.cc
@@ -13,7 +13,7 @@ void getHiggsCandidate(h_patJets inputJets, pat::Jet &d1, pat::Jet &d2){
   for(auto jet1=inputJets->begin(); jet1!=inputJets->end(); ++jet1){
     for(auto jet2=jet1+1; jet2!=inputJets->end() && jet1!=inputJets->end()-1; ++jet2){
       double pT2=pow(jet1->px()+jet2->px(),2)+pow(jet1->py()+jet2->py(),2);
-      if (pT2>maxPT2){
+      if (pT2>maxPT2 && fabs(jet1->p4().Eta())<2.5 && fabs(jet2->p4().Eta())<2.5 && jet1->p4().Pt()>20 && jet2->p4().Pt()>20){
 	d1=*jet1;
 	d2=*jet2;
       }
