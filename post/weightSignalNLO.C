80500d3045eb5dddf8ad5fe8b8627c087da9ac4f
sisagir <Sinan_Sagir@brown.edu>
2015-01-09 18:57:30 -0600

    Delete weightSignalNLO.C
    function moved into weightFunctions.C

--- post/weightSignalNLO.C
+++
@@ -1,9 +0,0 @@
-double weightSignalNLO(double pt){
-  double p0 = 0.9669;
-  double p1 = 0.0005825;
-  double p2 = -1.504E-6;
-  double p3 = 4.531E-10;
-
-  if(pt>400) pt=400;
-  return p0+p1*pt+p2*pow(pt,2)+p3*pow(pt,3);
-}
